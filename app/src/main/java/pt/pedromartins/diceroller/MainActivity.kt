3203ac3d081562c38b64745009e4134d69085518
Pedro Martins <rechousa@gmail.com>
2022-06-05 17:32:11 +0100

    Generating random value between 1 and 6

--- app/src/main/java/pt/pedromartins/diceroller/MainActivity.kt
+++ app/src/main/java/pt/pedromartins/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +19,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "Dice Rolled!"
+
+        val randomInt = Random().nextInt(6) + 1
+
+        resultText.text = randomInt.toString()
     }
 }
