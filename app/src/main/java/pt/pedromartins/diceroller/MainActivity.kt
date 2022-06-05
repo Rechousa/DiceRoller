af96db3fcfb8bc11ebd69ef554df9dd6b091ae6d
Pedro Martins <rechousa@gmail.com>
2022-06-05 17:48:06 +0100

    Drawing random images

--- app/src/main/java/pt/pedromartins/diceroller/MainActivity.kt
+++ app/src/main/java/pt/pedromartins/diceroller/MainActivity.kt
@@ -2,28 +2,37 @@ package pt.pedromartins.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    
-    private lateinit var resultText: TextView
+
+    private lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
+
         rollButton.setOnClickListener {
             rollDice()
         }
     }
 
     private fun rollDice() {
-        resultText = findViewById(R.id.result_text)
-
         val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
